8dfa695f894068f98837828ee006d05bb64ecc0a
Outute <iffiff1@gmail.com>
2011-11-02 10:31:09 +0800

    [fix test cases]

--- core/src/test/java/com/edu/service/impl/TutorialManagerImplTest.java
+++ core/src/test/java/com/edu/service/impl/TutorialManagerImplTest.java
@@ -6,6 +6,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.edu.dao.TutorialDao;
+import com.edu.dao.TutorialScheduleDao;
+import com.edu.dao.UserDao;
 import com.edu.model.Tutorial;
 import com.edu.service.impl.BaseManagerMockTestCase;
 import com.edu.service.impl.TutorialManagerImpl;
@@ -16,79 +18,94 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class TutorialManagerImplTest extends BaseManagerMockTestCase {
-    private TutorialManagerImpl manager = null;
-    private TutorialDao dao = null;
+	private TutorialManagerImpl manager = new TutorialManagerImpl();
+	private TutorialDao tutorialDao = null;
+	private TutorialScheduleDao tutorialScheduleDao = null;
+	private UserDao userDao = null;
 
-    @Before
-    public void setUp() {
-        dao = context.mock(TutorialDao.class);
-    }
+	@Before
+	public void setUp() {
+		tutorialDao = context.mock(TutorialDao.class);
+		tutorialScheduleDao = context.mock(TutorialScheduleDao.class);
+		userDao = context.mock(UserDao.class);
+		manager.setTutorialDao(tutorialDao);
+		manager.setTutorialScheduleDao(tutorialScheduleDao);
+		manager.setUserDao(userDao);
+	}
 
-    @After
-    public void tearDown() {
-        manager = null;
-    }
+	@After
+	public void tearDown() {
+		manager = null;
+	}
 
-    @Test
-    public void testGetTutorial() {
-        log.debug("testing get...");
+	@Test
+	public void testGetTutorial() {
+		log.debug("testing get...");
 
-        final Long id = 7L;
-        final Tutorial tutorial = new Tutorial();
+		final Long id = 1L;
+		final Tutorial tutorial = new Tutorial();
 
-        // set expected behavior on dao
-        context.checking(new Expectations() {{
-            one(dao).get(with(equal(id)));
-            will(returnValue(tutorial));
-        }});
+		// set expected behavior on dao
+		context.checking(new Expectations() {
+			{
+				one(tutorialDao).get(with(equal(id)));
+				will(returnValue(tutorial));
+			}
+		});
 
-        Tutorial result = manager.get(id);
-        assertSame(tutorial, result);
-    }
+		Tutorial result = manager.get(id);
+		assertSame(tutorial, result);
+	}
 
-    @Test
-    public void testGetTutorials() {
-        log.debug("testing getAll...");
+	@Test
+	public void testGetTutorials() {
+		log.debug("testing getAll...");
 
-        final List tutorials = new ArrayList();
+		final List tutorials = new ArrayList();
 
-        // set expected behavior on dao
-        context.checking(new Expectations() {{
-            one(dao).getAll();
-            will(returnValue(tutorials));
-        }});
+		// set expected behavior on dao
+		context.checking(new Expectations() {
+			{
+				one(tutorialDao).getAll();
+				will(returnValue(tutorials));
+			}
+		});
 
-        List result = manager.getAll();
+		List result = manager.getAll();
 
-        assertSame(tutorials, result);
-    }
+		assertSame(tutorials, result);
+	}
 
-    @Test
-    public void testSaveTutorial() {
-        log.debug("testing save...");
+	@Test
+	public void testSaveTutorial() {
+		log.debug("testing save...");
 
-        final Tutorial tutorial = new Tutorial();
-        // enter all required fields
-        
-        // set expected behavior on dao
-        context.checking(new Expectations() {{
-            one(dao).save(with(same(tutorial)));
-        }});
+		final Tutorial tutorial = new Tutorial();
+		// enter all required fields
 
-        manager.save(tutorial);
-    }
+		// set expected behavior on dao
+		context.checking(new Expectations() {
+			{
+				one(tutorialDao).save(with(same(tutorial)));
+			}
+		});
 
-    @Test
-    public void testRemoveTutorial() {
-        log.debug("testing remove...");
+		manager.save(tutorial);
+	}
 
-        final Long id = -11L;
+	@Test
+	public void testRemoveTutorial() {
+		log.debug("testing remove...");
 
-        // set expected behavior on dao
-        context.checking(new Expectations() {{
-            one(dao).remove(with(equal(id)));
-        }});
+		final Long id = -11L;
 
-        manager.remove(id);
-    }
+		// set expected behavior on dao
+		context.checking(new Expectations() {
+			{
+				one(tutorialDao).remove(with(equal(id)));
+			}
+		});
+
+		manager.remove(id);
+	}
 }
